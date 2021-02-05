ac061fa78729c230b7ec178e3b81fd014e7ae968
kanishka424 <kanishkamalpana24@gmail.com>
2021-02-05 09:25:29 +0530

    first commit

---
+++ src/main/java/com/example/fluffy/springsecuritycodejava/SpringsecuritycodejavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.fluffy.springsecuritycodejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritycodejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritycodejavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fluffy/springsecuritycodejava/SpringsecuritycodejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fluffy.springsecuritycodejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritycodejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
